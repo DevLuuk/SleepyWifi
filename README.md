24e474b6daec58cc0a5b935f2c5c95ba2b1ef17a
DevLuuk <devluuk@gmail.com>
2018-08-16 16:17:56 +0200

    changed appcompatactivity to activity inside mainactivity and updated the readme

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # SleepyWifi
-======
 This Android app brings back the 'turn wifi off when the phone is in sleepmode' option for Android 8.1.
 this app works for Android devices with version 8.0 or higher.
 
 ## License
-======
 SleepyWifi is licensed under GPL-3.0 or later.
 
 ## Author
-======
 SleepyWifi is written by [Devluuk](https://www.luukpeterse.nl "Devluuk Homepage")
--- app/src/main/java/nl/devluuk/sleepywifi/MainActivity.java
+++ app/src/main/java/nl/devluuk/sleepywifi/MainActivity.java
@@ -1,14 +1,14 @@
 package nl.devluuk.sleepywifi;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
