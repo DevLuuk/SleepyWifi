da5eeec0e6375aea3877a2e6068c9fa720efaaf7
DevLuuk <devluuk@gmail.com>
2018-12-10 11:21:06 +0100

    add onstart notification for 5 seconds for android 8.0 or higher

--- app/src/main/java/nl/devluuk/sleepywifi/OnStart.java
+++ app/src/main/java/nl/devluuk/sleepywifi/OnStart.java
@@ -5,6 +5,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
@@ -13,9 +14,12 @@ public class OnStart extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            context.startService(new Intent(context, BackgroundService.class));
+            Intent i = new Intent(context, BackgroundService.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                context.startForegroundService(i);
+            } else {
+                context.startService(i);
+            }
         }
-//        Intent serviceIntent = new Intent(context, BackgroundService.class);
-//        ContextCompat.startForegroundService(context, serviceIntent );
     }
 }
