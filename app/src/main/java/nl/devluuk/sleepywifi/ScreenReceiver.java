d5ae0d8fea59f6e28d03330a041fffe07d0f075f
Luuk Peterse <l.peterse@wearetriple.com>
2019-03-19 22:28:05 +0100

    Added 5 seconds delay for turning off wifi when the screen is turned off And Cleaned up code

--- app/src/main/java/nl/devluuk/sleepywifi/ScreenReceiver.java
+++ app/src/main/java/nl/devluuk/sleepywifi/ScreenReceiver.java
@@ -8,12 +8,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
+
+import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
 
 public class ScreenReceiver extends BroadcastReceiver {
 
     private WifiManager wifiManager;
+    private PowerManager powerManager;
     private BluetoothAdapter bluetoothAdapter;
     boolean wifiWasOn = false;
     boolean bluetoothWasOn = false;
@@ -22,22 +28,19 @@ public class ScreenReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent intent) {
-
         wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         bluetoothState = prefs.getBoolean(context.getResources().getString(R.string.bluetooth_state), false);
 
         PackageManager pm = context.getPackageManager();
         final boolean deviceHasBluetooth = pm.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
 
-        //boolean wifiEnabled = wifiManager.isWifiEnabled();
-
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
             if (wifiManager.isWifiEnabled()) {
-                wifiManager.setWifiEnabled(false);
+                new Sleep(this).execute();
                 wifiWasOn = true;
-
             } else {
                 wifiWasOn = false;
             }
@@ -51,7 +54,6 @@ public class ScreenReceiver extends BroadcastReceiver {
                         bluetoothWasOn = false;
                     }
                 }
-
             }
         } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
             if (wifiWasOn) {
@@ -64,6 +66,25 @@ public class ScreenReceiver extends BroadcastReceiver {
                     }
                 }
             }
+        }
+    }
+    private class Sleep extends AsyncTask<Void, Void, Void> {
+
+        private WeakReference<ScreenReceiver> activityReference;
+
+        // only retain a weak reference to the activity
+        Sleep(ScreenReceiver context) {
+            activityReference = new WeakReference<>(context);
+        }
+
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                wifiManager.setWifiEnabled(false);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 
--- app/src/main/java/nl/devluuk/sleepywifi/SettingsFragment.java
+++ app/src/main/java/nl/devluuk/sleepywifi/SettingsFragment.java
@@ -23,7 +23,5 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_visualizer);
-
-
     }
 }
