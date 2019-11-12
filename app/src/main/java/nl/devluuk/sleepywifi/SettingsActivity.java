f7383a1bf76e4ebce2fd12bf4efab8957a9ebe80
DevLuuk <devluuk@gmail.com>
2019-11-12 21:21:18 +0100

    Removed Sleep delay function and removed 'Keep wifi on when phone is plugged in' function

--- app/src/main/java/nl/devluuk/sleepywifi/SettingsActivity.java
+++ app/src/main/java/nl/devluuk/sleepywifi/SettingsActivity.java
@@ -1,9 +1,9 @@
 package nl.devluuk.sleepywifi;
 
 import android.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
