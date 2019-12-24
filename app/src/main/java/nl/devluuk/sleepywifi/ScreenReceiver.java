d4cbdf811db565eac7387c194b96c73bca27e08b
DevLuuk <devluuk@gmail.com>
2019-12-24 19:59:55 +0100

    Cleaned up delay app service sleeping settings

--- app/src/main/java/nl/devluuk/sleepywifi/ScreenReceiver.java
+++ app/src/main/java/nl/devluuk/sleepywifi/ScreenReceiver.java
@@ -40,7 +40,9 @@ public class ScreenReceiver extends BroadcastReceiver {
         if (appState) {
             if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
                 if (wifiManager.isWifiEnabled()) {
-                    new Sleep(this).execute();
+                    if(delayTime != 0) {
+                        new Sleep(this).execute();
+                    }
                     wifiManager.setWifiEnabled(false);
                     wifiWasOn = true;
                 } else {
